fbb970e7357c8b9b6c469dd447387bf77210c060
alexup71rus <42513019+alexup71rus@users.noreply.github.com>
2019-07-31 20:54:06 +0300

    build files

---
+++ docs/precache-manifest.2ac6a82bdd21da728f49130eabcb678e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab11192fd42d9789adcb7eb46dd82bf5",
+    "url": "/static/media/291212.ab11192f.svg"
+  },
+  {
+    "revision": "cd576f60f78e90648dce",
+    "url": "/static/css/main.660edba1.chunk.css"
+  },
+  {
+    "revision": "fef33eebb17d280055e2",
+    "url": "/static/js/1.fef33eeb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5fc4451647fa64227db6f1983e045add",
+    "url": "/static/media/61731.5fc44516.svg"
+  },
+  {
+    "revision": "cd576f60f78e90648dce",
+    "url": "/static/js/main.cd576f60.chunk.js"
+  },
+  {
+    "revision": "eceedf32373ce27e9f7a9ce3243ab221",
+    "url": "/static/media/logo2.eceedf32.svg"
+  },
+  {
+    "revision": "c4c396d3afb908f878b4835a616ba1af",
+    "url": "/static/media/close.c4c396d3.svg"
+  },
+  {
+    "revision": "2e61c3f50fa6a7a1abd423f0a196bc90",
+    "url": "/static/media/126488.2e61c3f5.svg"
+  },
+  {
+    "revision": "9657789f9af2602bae9e661765b3926b",
+    "url": "/static/media/MaskFigureUnregister2.9657789f.svg"
+  },
+  {
+    "revision": "fb48d3f9010724c78ca85dab82247472",
+    "url": "/index.html"
+  }
+];
