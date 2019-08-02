06948a29ce76425bb55cf0119448cf0f9e54366c
alexup71rus <42513019+alexup71rus@users.noreply.github.com>
2019-08-02 19:52:38 +0300

    build pwa

--- public/worker.js
+++ public/worker.js
@@ -1,7 +1,8 @@
-var CACHE_NAME = 'pwa-task-manager';
+var CACHE_NAME = 'pwa-unsplash-viewer';
 var urlsToCache = [
     '/',
-    '/static'
+    '/static',
+    'completed'
 ];
 
 // Install a service worker
@@ -46,31 +47,4 @@ self.addEventListener('activate', event => {
         })
     );
 });
-
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-});
-
-async function install() {
-    if (deferredPrompt) {
-        deferredPrompt.prompt();
-        console.log(deferredPrompt)
-        deferredPrompt.userChoice.then(function (choiceResult) {
-
-            if (choiceResult.outcome === 'accepted') {
-                console.log('Your PWA has been installed');
-            } else {
-                console.log('User chose to not install your PWA');
-            }
-
-            deferredPrompt = null;
-
-        });
-
-
-    }
-}
 
